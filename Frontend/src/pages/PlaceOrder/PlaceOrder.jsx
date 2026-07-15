1718c8197c85b870fc7eafaac583b352dc5941b1
Varsharani108 <Varsharanibgs108@gmail.com>
2026-07-15 22:41:07 +0530

    Add cart page and cart total functionality

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,7 +3,22 @@ import './PlaceOrder.css'
 
 function PlaceOrder() {
   return (
-    <div>PlaceOrder</div>
+    <div>
+      <form className ='place-order'>
+        <div className="place-order-left">
+          <p className='title'>Delivery Information</p>
+
+        </div>
+        <div className="multi-field">
+          <input type="text" placeholder='First name'/>
+          <input type="text" placeholder='Last name'/>
+
+        </div>
+        <div className="place-order-right">
+
+        </div>
+      </form>
+    </div>
   )
 }
 
